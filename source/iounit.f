76d8fae9bf27bac62d6d1918608f659c60bbc069
Mario Antonioletti <mario@epcc.ed.ac.uk>
2014-07-01 16:50:37 +0100

    Moved the unit number ikey to iounit.f and removed calls to freeunit.

--- source/iounit.f
+++ source/iounit.f
@@ -17,5 +17,6 @@ c
       integer, parameter:: input =  5 ! Fortran I/O unit for main input (default=5)
       integer, parameter:: iout  =  6 ! Fortran I/O unit for main output (default=6)
       integer, parameter:: ixyz  = 10 ! Fortran I/O unit for xyz files.
+      integer, parameter:: ikey  = 11 ! Fortran I/O unit for key files.
       save
       end
