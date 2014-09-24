3dfc25af261a4422c0790bb678ce0bc08bae9db2
Mario Antonioletti <mario@epcc.ed.ac.uk>
2014-09-24 14:47:54 +0100

    atoms.f: changed the internal documentation.

--- source/atoms.f
+++ source/atoms.f
@@ -12,34 +12,26 @@ c     ##                                                        ##
 c     ############################################################
 c
 c
-c     n       total number of atoms in the current system
-c     type    atom type number for each atom in the system
-c     x       current x-coordinate for each atom in the system
-c     y       current y-coordinate for each atom in the system
-c     z       current z-coordinate for each atom in the system
-c
-c
 
       module atoms
       use sizes
 
       implicit none
 
-      integer:: n
+      integer:: n ! total number of atoms in the current system
 
 c      integer:: type(maxatm)
 
-
       type atomtype
-      real(kind=8), dimension(3) :: pos
-      real(kind=8) :: mass
-      integer :: tag
-      integer :: type
-      integer :: class 
-      integer :: atomic
-      integer :: valence 
-      character(len=24) :: story
-      character(len=3) :: name
+      real(kind=8), dimension(3) :: pos  ! Atom position
+      real(kind=8) :: mass               ! Atomic mass
+      integer :: tag                     ! Atom label from input coord file
+      integer :: type                    ! Atom type number
+      integer :: class                   ! Atom class number
+      integer :: atomic                  ! Atomic number
+      integer :: valence                 ! Valence number for atom
+      character(len=24) :: story         ! Descriptive type for atom
+      character(len=3) :: name           ! Atom name
       end type atomtype
 
       save
