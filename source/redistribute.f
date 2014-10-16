9ee18cf8a566c2b5b2aab9bd61bee8794e8027f5
Mario Antonioletti <mario@epcc.ed.ac.uk>
2014-10-16 10:37:16 +0100

    redistribute.f: improved the internal documentation.

--- source/redistribute.f
+++ source/redistribute.f
@@ -34,6 +34,8 @@
       lminbox(3) = minval(atom%pos(3))
 
       ! Find the global system extent
+      ! NB this may be dictated by input parameters in the key 
+      ! file or in the coordinate file.
       call MPI_Allreduce(lminbox,minbox,3, MPI_DOUBLE_PRECISION, 
      &                   MPI_MIN, MPI_COMM_WORLD, ierror)
   
@@ -46,25 +48,29 @@
       ! Number of recursive splits possible
       nsplits = int(ceiling(log2(real(nprocs,kind=8))))
 
-      ! Store informaiton about: 
-      !                         split number, 
-      !                         split direction, 
-      !                         split coord, 
-      !                         neighbouring process
-      !                         system box for the split
+      ! Structure to store information about:
+      ! 
+      !  split number 
+      !  split direction
+      !  rank of neighbour in other domain to communicate with 
+      !  "above" or "below" in the domain split
+      !  communicator for each subdomain
+      !  split coord 
+      !  min and max coord of the subdomain system box
+      !
       if(.not. allocated(splits)) then
          allocate(splits(nsplits))
       end if
 
-      ! Set the the min/max for the current domain.
+      ! Set the the min/max for the top domain.
       splits(1)%minbox = minbox
       splits(1)%maxbox = maxbox
 
-      ! Loop over the number of splits
+      ! Loop over the recursive bisections
       do ns=1, nsplits
 
-        ! Set the extent of the domain to be the 
-        ! extent of the previous domain.
+        ! To start set the extent of the current domain 
+        ! to be the extent of the previous domain.
         if(ns.gt.1) then 
           splits(ns)%minbox = splits(ns-1)%minbox
           splits(ns)%maxbox = splits(ns-1)%maxbox
@@ -73,42 +79,52 @@
         ! Calculate the local domain extent.
         lsysbox = splits(ns)%maxbox - splits(ns)%minbox
 
-        ! Determine the comms channel - use this to determin
-        ! neighbouring process and what side of the split 
-        ! this process belongs to.
+        ! Determine the comms channel - use this construct to determine
+        ! neighbouring process and what side of the split this process
+        ! belongs to.
         chan = ishft(1,(ns-1))
 
         ! Deteremine what the neighbouring process rank is - this
-        ! is based on a Gray code. Note ieor(neighbor,split)->rank.
+        ! is based on a Gray code. Note that applying the inverse
+        ! returns the original rank, i.e. ieor(neighbor,split)->rank.
         splits(ns)%neighbor = ieor(rank,chan)
 
-        ! Determine whether we are above or below the split.
-        ! This will return a 0 or a 1.
+        ! Determine whether we are "above" or "below" the 
+        ! split. This will return a 0 or a 1. Assign the 
+        ! upper coordinates to the "above" processes. 
         splits(ns)%above = ishft(iand(rank,chan),-(ns-1))
 
         ! Create a subcommunicator for each of the subdomains
-        ! so that we can carry out collective communications
-        ! for that subdomain. Only want these for collective
-        ! operations - point-to-point operations will still 
-        ! operate using MPI_COMM_WORLD.
+        ! to enable collective communications for the subdomain.
+        ! Only want these communicators for collective
+        ! operations - point-to-point operations must still 
+        ! operate with MPI_COMM_WORLD.
         if(ns.gt.1) then 
            call MPI_Comm_split(splits(ns-1)%comm,
      &                         splits(ns)%above,
      &                         rank,
      &                         splits(ns)%comm, ierror)
         else
+           ! For the first split default to the global coordinate
            splits(ns)%comm=MPI_COMM_WORLD
         end if
 
-        ! Determine the splitting direction - use the 
-        ! longest direction
+        ! Determine the splitting direction, i.e. split 
+        ! along the longest systems direction
         splits(ns)%splitdir = maxloc(lsysbox, dim=1)
 
-        ! Determine the splitting point
+        ! Determine the splitting coordinate
         splits(ns)%splitcoord=findSplit(splits(ns)%splitdir,
      &                                  splits(ns)%minbox,
      &                                  splits(ns)%maxbox,
      &                                  splits(ns)%comm)
+
+      ! sort local atoms along the splitting direction
+      ! NB can only do this the first time otherwise all
+      ! all the atom relationships will break once these
+      ! have been established.
+      call sortAtoms(splits(ns)%splitdir, 1, dn)
+
 
         !if(lrank.eq.0) then
           print "(A,I3,A,I2,A,F7.3)","Split: ",ns,
