4b6023df0726e50d33a8a08a4480e0f8bed0935b
Mario Antonioletti <mario@epcc.ed.ac.uk>
2015-01-26 09:52:26 +0000

    usage.f: restored what was in the master branch. Had a name clash.

--- source/usage.f
+++ source/usage.f
@@ -1,46 +1,26 @@
-c     Print a usage message.
 c
-
-      subroutine usage
-
-      use iounit 
-      use parallelparams
-
+c
+c     ###################################################
+c     ##  COPYRIGHT (C)  1992  by  Jay William Ponder  ##
+c     ##              All Rights Reserved              ##
+c     ###################################################
+c
+c     ################################################################
+c     ##                                                            ##
+c     ##  module usage  --  atoms active during energy computation  ##
+c     ##                                                            ##
+c     ################################################################
+c
+c
+c     nuse   total number of active atoms in energy calculation
+c     iuse   numbers of the atoms active in energy calculation
+c     use    true if an atom is active, false if inactive
+c
+c
+      module usage
       implicit none
-
-      ! Only want process 0 to print out the usage.
-      if(rank.eq.0) then 
-
-      write (iout,*)
-      write (iout,*) " Usage: "
-      write (iout,*)
-      write (iout,*) "  dynamic [file] [nstep] [dt] [dtdump] [temp]", 
-     &               " [pres]"
-      write (iout,*)
-      write (iout,*) " where:"
-      write (iout,*) 
-      write (iout,*) " file   : base name for .xyz coordinate file for",
-     &               " the simulation system."
-      write (iout,*) " nstep  : integer number of steps to be taken."
-      write (iout,*) " dt     : real number value of timestep length",
-     &               " in femtoseconds."
-      write (iout,*) " dtdump : real number value of time between data",
-     &               " writes in picoseconds."
-      write (iout,*) " temp   : real number value of temperature of",
-     &               " the system in Kelvin."
-      write (iout,*) " pres   : real number value of pressure of the",
-     &               " system in atmospheres."
-      write (iout,*)
-      write (iout,*) "Other valid flags that can be used are:"
-      write (iout,*)
-      write (iout,*) "-K keyfile : to specify the key file."
-      write (iout,*) "-V : sets verbose mode."
-      write (iout,*) "-D : sets debug mode (also switches on",
-     &               " verbose mode)."
-      write (iout,*)
-
-      end if 
-
-      end subroutine usage
-
- 
+      integer nuse
+      integer, allocatable :: iuse(:)
+      logical, allocatable :: use(:)
+      save
+      end
