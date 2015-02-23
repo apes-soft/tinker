38066045a976dda40b858bc335503dd1094a786b
Mario Antonioletti <mario@epcc.ed.ac.uk>
2015-02-23 14:24:16 +0000

    mpiparams.f: added a routine that will split loop bounds based on cost.

--- source/mpiparams.f
+++ source/mpiparams.f
@@ -14,7 +14,40 @@
 
       contains
 
+      subroutine dolimits(lstart, lend, cost)
+      integer, intent(out):: lstart              ! loop start
+      integer, intent(out):: lend                ! loop end
+      integer, dimension(:), intent(in):: cost   ! cost array
+      integer:: totcost                          ! total cost
+      integer:: avgcost                          ! average cost
+      integer:: partcost                         ! partial cost
 
+      totcost = sum(cost)
+      avgcost = totcost/nprocs
+
+      lstart = 1
+      partcost = 0
+
+      if(rank.gt.0) then 
+         do while(partcost.lt.(rank*avgcost+1)) 
+            partcost = partcost + cost(lstart)
+            lstart = lstart + 1
+         end do
+      end if
+
+      if(rank.eq.nprocs-1) then 
+         lend = size(cost)
+      else
+         lend = lstart+1
+         do while(partcost.lt.((rank+1)*avgcost+1))
+            partcost = partcost + cost(lend)
+            lend   = lend + 1
+         end do
+      end if
+
+      print *,rank," start ",lstart," end ",lend," out of ",size(cost)
+
+      end subroutine 
 
       end module mpiparams
 
