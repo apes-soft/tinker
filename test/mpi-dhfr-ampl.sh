5747167c3f4c4be0abe75184d29edd28ed9eddb3
Mario Antonioletti <mario@epcc.ed.ac.uk>
2015-05-21 13:21:45 +0100

    mpi-dhfr-ampl.sh: allow the number of procs to be specified as a command line arg.

--- test/mpi-dhfr-ampl.sh
+++ test/mpi-dhfr-ampl.sh
@@ -4,17 +4,41 @@
 # Press(Atms)
 #
 
+if [ "$#" -gt 1 ] 
+then 
+
+    # Print usage information
+    echo
+    echo Usage: $0 NumProcs 
+    echo 
+    echo If only one number is specified that is used for the
+    echo number of processes, if no numbers are specified the
+    echo value defaults to 1.
+    echo
+
+    # ... and exit
+    exit
+
+elif [ "$#" -eq 1 ]
+then
+
+   nprocs=$1
+
+else
+
+   nprocs=1
+
+fi
+
 # Need to take care of slurm
 export SLURM_JOBID=
-
-# Number of processes to be used
-nprocs=16
 
 # File to output to
 outfile="out-ampl.txt"
 
 # Add some empty lines
-echo \n\n >> $outfile
+printf "\n\nUsing $nprocs processes\n\n" | tee -a $outfile
+
 
 (time mpirun -np $nprocs \
             amplxe-cl -r S2_$nprocs -collect hotspots -- \
