39525e4cb8348345074e776bcef96de3bfb046e5
Mario Antonioletti <mario@epcc.ed.ac.uk>
2015-07-16 10:45:21 +0100

    Pulled in the version of mpi-dhfr.sh from repmem.

--- test/mpi-dhfr.sh
+++ test/mpi-dhfr.sh
@@ -9,20 +9,60 @@
 # Can add `time` in front to time execution. If NumOfProcs 
 # is not specified it defaults to 1.
 
-# Check if a number of processes has been 
+# Check if a number of processes or threads has been 
 # specified in the command line otherwise
-# default to 1.
-if [ "$#" -eq 1 ] 
+# default to 1 for each case.
+if [ "$#" -gt 2 ] 
 then 
+
+    echo
+    echo Usage: $0 NumProcs NumThreads
+    echo 
+    echo If only one number is specified that is used for the
+    echo number of processes, if no numbers are specified the
+    echo values default to 1.
+    echo
+    exit
+
+elif [ "$#" -eq 2 ] 
+then 
+
    nprocs=$1
+   nthreads=$2
+
+elif [ "$#" -eq 1 ]
+then
+
+   nprocs=$1
+   nthreads=1
+
 else
+
    nprocs=1
+   nthreads=1
+
 fi
 
-echo Using $nprocs processes.
+# Unlimit the stack
+ulimit -s unlimited
 
-export OMP_NUM_THREADS=1
-echo Explicitly setting OMP_NUM_THREADS to $OMP_NUM_THREADS
+# Create an OpenMP stack
+export OMP_STACKSIZE=200M
+
+# Outfile name
+outfile="out.txt"
+
+# Dynamic command
+dynamic="../bin/dynamic dhfr 10 1.0 10.0 2 300.0" 
+
+echo | tee -a $outfile                           # Add an empty line
+echo Using $nprocs processes. | tee -a $outfile  # How many procs are usd
+echo Started run on `date`. | tee -a $outfile  # How many procs are usd
+
+export OMP_NUM_THREADS=$nthreads
+echo Explicitly setting OMP_NUM_THREADS to $OMP_NUM_THREADS. | tee -a $outfile
+
+#echo processes $nprocs, threads $nthreads
 
 # Command line arguments to dynamic:
 #
@@ -36,9 +76,12 @@ echo Explicitly setting OMP_NUM_THREADS to $OMP_NUM_THREADS
 # Possible arguments for mpirun:
 #
 # -np - number of processes
-# -mca btl tcp,sm,self - specify the message transport mechanism (for Indy)
-# --mca orte_base_help_aggregate 0  - disable error message aggregation (for Indy)
 # --quiet - don't print out so much stuff out
+#
+# For indy:
+# --mca btl tcp,sm,self - specify the message transport mechanism 
+# --mca orte_base_help_aggregate 0  - disable error message aggregation
+
 
 # Really want to use `hostname -s` but not universally available.
 # For debugging can add the line: xterm -e gdb before the executable
@@ -46,23 +89,20 @@ echo Explicitly setting OMP_NUM_THREADS to $OMP_NUM_THREADS
 
 if [ `hostname` = "mbp-ma.local" ]; then  # Mario's mac.
 
-  mpirun -np $nprocs --quiet \
-         ../bin/dynamic dhfr 100 1.0 10.0 2 300.0 \
-         > >(tee out) 2>&1
+  (time mpirun -np $nprocs --quiet $dynamic) 2>&1\
+         | tee -a $outfile
 
 elif [ `hostname` = "mbp-ma.lan" ]; then  # Mario's mac.
 
-  mpirun -np $nprocs --quiet \
-         ../bin/dynamic dhfr 100 1.0 10.0 2 300.0 \
-         > >(tee out) 2>&1
+  (time mpirun -np $nprocs --quiet $dynamic) 2>&1\
+         | tee -a $outfile
 
 elif [ `hostname` = "indy0" ]; then       # Indy (system at EPCC).
 
 
-  mpirun -np $nprocs \
-         -mca btl tcp,sm,self \
-         ../bin/dynamic dhfr 10 1.0 10.0 2 300.0 \
-         > >(tee out) 2>&1
+  (time mpirun -np $nprocs \
+         -mca btl tcp,sm,self $dynamic) 2>&1\
+         | tee -a $outfile 
 
 elif [ `hostname` = "phi.hydra" ]; then       # phi.hydra (system at EPCC).
 
@@ -70,9 +110,10 @@ elif [ `hostname` = "phi.hydra" ]; then       # phi.hydra (system at EPCC).
   export SLURM_JOBID=
 
   # Run the code
-  time mpirun -np $nprocs \
-         ../bin/dynamic dhfr 100 1.0 10.0 2 300.0 \
-         > >(tee out) 2>&1
+  #(time mpirun -trace -np $nprocs $dynamic) 2>&1 \
+  #(time mpirun -np $nprocs inspxe-cl -collect mi2 -r rmi3 $dynamic) 2>&1\
+  (time mpirun -np $nprocs $dynamic) 2>&1\
+         | tee -a $outfile
 
 else
 
@@ -81,11 +122,9 @@ echo "Unknown system :" `hostname`.
 echo " Check MPI call routine."
 echo
 
-  mpirun -np $nprocs \
-         -mca btl tcp,sm,self \
-         ../bin/dynamic dhfr 2 1.0 10.0 2 300.0 \
-         > >(tee out) 2>&1
-
+  (time mpirun -np $nprocs \
+         -mca btl tcp,sm,self $dynamic) 2>&1\
+         | tee -a $outfile
 
 fi
 
